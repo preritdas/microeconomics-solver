c32e9ae357553e466067c4f226fd4c36c9fed7ef
Prerit Das <preritdas@gmail.com>
2023-09-05 21:01:44 -0400

    Keys

---
+++ keys/models.py
@@ -0,0 +1,12 @@
+"""Models for secrets."""
+from pydantic import BaseModel
+
+
+class OpenAIModel(BaseModel):
+    """API key for OpenAI."""
+    api_key: str
+
+
+class Keys(BaseModel):
+    """Keys for various APIs."""
+    openai: OpenAIModel
