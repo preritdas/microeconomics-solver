956c46b88a85403dbb6724ef15f671a387e0325b
Prerit Das <preritdas@gmail.com>
2023-09-05 21:05:45 -0400

    Base structure.

--- solver/__init__.py
+++ solver/__init__.py
@@ -7,3 +7,19 @@ from keys import KEYS
 
 
 llm = ChatOpenAI(openai_api_key=KEYS.OpenAI.api_key)
+agent = ZeroShotAgent.from_llm_and_tools(
+    llm=llm,
+    tools=None
+)
+executor = AgentExecutor(
+    agent=agent,
+    tools=None, # YO
+    max_iterations=5,
+    verbose=True,
+    callbacks=None # YO
+)
+
+
+def solve(question: str) -> str:
+    """Solve the question."""
+    return executor.run(question)
