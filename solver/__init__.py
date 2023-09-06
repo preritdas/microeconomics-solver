344eab78f732a10d5bc979c33efec95bc5b1e2f4
Prerit Das <preritdas@gmail.com>
2023-09-05 22:06:50 -0400

    Solve with streamlit callback if necessary.

--- solver/__init__.py
+++ solver/__init__.py
@@ -2,6 +2,7 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import ZeroShotAgent
 from langchain.agents import AgentExecutor
+from langchain.callbacks import StreamlitCallbackHandler
 
 from solver.prompts import PREFIX, FORMAT_INSTRUCTIONS
 from solver.tools import TOOLKIT
@@ -19,11 +20,13 @@ executor = AgentExecutor(
     agent=agent,
     tools=TOOLKIT,
     max_iterations=10,
-    verbose=True,
-    callbacks=None  # TODO
+    verbose=True
 )
 
 
-def solve(question: str) -> str:
+def solve(question: str, container = None) -> str:
     """Solve the question."""
+    if container:
+        return executor.run(question, callbacks=StreamlitCallbackHandler(container))
+
     return executor.run(question)
