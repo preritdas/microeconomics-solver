5f690040c1e1a6051e4f8b82cd546d6246d9550b
Prerit Das <preritdas@gmail.com>
2023-09-07 20:08:28 +0000

    Proper reading.

--- textbook/__init__.py
+++ textbook/__init__.py
@@ -1,20 +1,27 @@
 """Read and vectorize the textbook for this course."""
-from unstructured.partition.pdf import partition_pdf
+import PyPDF2
 
 from pathlib import Path
 
 
-def _convert(file_path: str) -> str:
-    """Convert a PDF file to text. Assumes text-readable PDF."""
-    elements = partition_pdf(file_path)
+def pdf_to_text(pdf_path, output_txt_path):
+    # Open the PDF file in binary read mode
+    with open(pdf_path, 'rb') as file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(pdf_path)
+        
+        # Extract text from each page
+        full_text = ""
+        for page in pdf_reader.pages:
+            full_text += page.extract_text()
+        
+    # Save the extracted text to the output text file
+    with open(output_txt_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(full_text)
 
-    text: str = ""
-    for element in elements:
-        text += " " + element.text
 
-    return text
-
-
-def read_textbook(path: Path = Path.cwd() / "textbook.pdf") -> str:
+def read_textbook() -> str:
     """Read the textbook and return it as a string."""
-    return _convert(str(path.absolute()))
+    input_path = Path(__file__).parent.absolute() / "textbook.pdf"
+    output_path = Path(__file__).parent.absolute() / "textbook.txt"
+    return pdf_to_text(input_path, output_path)
