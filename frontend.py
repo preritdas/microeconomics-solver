16c8ec748e283424831c978dcce3b374dad3c96a
Prerit Das <preritdas@gmail.com>
2023-09-08 05:33:55 +0000

    Improve frontend look a bit.

--- frontend.py
+++ frontend.py
@@ -1,21 +1,29 @@
-"""Interface."""
+"""Frontend interface for AlphaDenti."""
 import streamlit as st
-
 from pathlib import Path
 from solver import solve
 
+# Theme and Styling
+st.set_page_config(
+    page_title="AlphaDenti",
+    page_icon=":bulb:" 
+)
 
-st.title("AlphaDenti")
-st.subheader("An AI-powered problem solver for Micro with Calc.")
+# Header
+st.title(":brain: AlphaDenti")
+st.info(
+    ":wave: \tHey. I'm an AI designed to help you with Microeconomics with Calculus.\n\n"
+    ":book: I have access to the textbook, can do math, create plots, research, and more.\n\n"
+    ":teacher: Use me to ask follow-up questions, check your work, or just to learn more.\n\n"
+)
+st.divider()
 
-
-question = st.text_input("Paste the problem here.", value="Who are you?")
+# Body
+question = st.text_input("Ask a question/paste a problem.", value="What does the textbook say about market equilibrium? Show me visually.")
 button = st.button("Solve")
 
 if question and button:
     response = solve(question, st.container())
-
-    # Display answer
     st.divider()
     st.markdown(response)
 
