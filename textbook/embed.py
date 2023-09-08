cd407cbf8f6eb20fb109d05a568eb938b5b5fe5a
Prerit Das <preritdas@gmail.com>
2023-09-08 01:27:27 +0000

    Do it with FAISS!

--- textbook/embed.py
+++ textbook/embed.py
@@ -1,41 +1,33 @@
 """Embed the textbook text and save in Pinecone."""
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import TokenTextSplitter
-from langchain.vectorstores import Pinecone
+from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
 from langchain.schema import Document
 
-import pinecone
 from pathlib import Path
 
 from keys import KEYS
 
 
+textbook_path = Path(__file__).parent.absolute() / "textbook.txt"
 embeddings = OpenAIEmbeddings(openai_api_key=KEYS.OpenAI.api_key)
-splitter = TokenTextSplitter(encoding_name="cl100k_base", chunk_size=450, chunk_overlap=50)
 
 
 def load_docs() -> list[Document]:
     """Load in the textbook as split documents."""
-    textbook_path = Path(__file__).parent.absolute() / "textbook.txt"
-    loader = TextLoader(textbook_path)
+    splitter = TokenTextSplitter(encoding_name="cl100k_base", chunk_size=450, chunk_overlap=50)
+    loader = TextLoader(str(textbook_path))
     docs = loader.load()
     return splitter.split_documents(docs)
 
 
-def embed_to_pinecone(docs: list[Document]) -> bool:
+def embed_to_file(docs: list[Document]) -> bool:
     """Embed the docs and upload to Pinecone."""
-    pinecone.init(
-        api_key=KEYS.Pinecone.api_key,
-        environment="gcp-starter",
-    )
+    vs = FAISS.from_documents(docs, embeddings)
+    vs.save_local(str(textbook_path.parent / "vectorstore"))
 
-    index_name = "textbook"
-    if index_name not in pinecone.list_indexes():
-        pinecone.create_index(
-        name=index_name,
-        metric='cosine',
-        dimension=1536  
-    )
 
-    docsearch = Pinecone(pinecone.Index(index_name))
+def load_vectorstore() -> FAISS:
+    """Load the vectorstore from file."""
+    return FAISS.load_local(str(textbook_path.parent / "vectorstore"), embeddings)
