646c41b0c0f9c83fa823987045cf095174a215e6
Prerit Das <preritdas@gmail.com>
2023-09-08 05:14:39 +0000

    Use custom prompt.

--- solver/interpreter/__init__.py
+++ solver/interpreter/__init__.py
@@ -6,10 +6,12 @@ import os; os.environ["CODEBOX_API_KEY"] = KEYS.CodeBox.api_key
 from codeinterpreterapi import CodeInterpreterSession, settings
 
 from pathlib import Path
+from solver.interpreter.prompt import system_message
 
 
 # Set the API keys and configs
 settings.OPENAI_API_KEY = KEYS.OpenAI.api_key
+settings.SYSTEM_MESSAGE = system_message
 settings.MODEL = "gpt-4"
 
 
