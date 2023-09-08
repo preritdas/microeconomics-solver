898f748b997ad7d23b48beba3818b4b8aa41ce5a
Prerit Das <preritdas@gmail.com>
2023-09-08 04:52:11 +0000

    First pass at running with interpreter.

---
+++ solver/interpreter.py
@@ -0,0 +1,29 @@
+"""Code Interpreter tool for plotting."""
+# Set CodeBox API key first
+from keys import KEYS
+import os; os.environ["CODEBOX_API_KEY"] = KEYS.CodeBox.api_key
+
+from codeinterpreterapi import CodeInterpreterSession, settings
+
+from pathlib import Path
+
+
+# Set the API keys and configs
+settings.OPENAI_API_KEY = KEYS.OpenAI.api_key
+settings.MODEL = "gpt-4"
+
+
+# Create the interpreter_files directory if it doesn't exist
+if not Path("interpreter_files").exists():
+    Path("interpreter_files").mkdir()
+
+
+def run(query: str) -> str:
+    """Run the query."""
+    with CodeInterpreterSession() as session:
+        response = session.generate_response(query)
+
+    for res_file in response.files:
+        res_file.save(f"interpreter_files/{res_file.name}")
+
+    return response.content
