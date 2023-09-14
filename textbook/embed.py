899175d3ccb2299e9d5671de2d49b009afbde368
Prerit Das <preritdas@gmail.com>
2023-09-14 15:58:49 +0000

    Load PDF and append page number.

--- textbook/embed.py
+++ textbook/embed.py
@@ -1,8 +1,8 @@
-"""Embed the textbook text and save in Pinecone."""
+"""Embed the textbook text and save in local FAISS."""
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import TokenTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.schema import Document
 
 from pathlib import Path
@@ -10,16 +10,21 @@ from pathlib import Path
 from keys import KEYS
 
 
-textbook_path = Path(__file__).parent.absolute() / "textbook.txt"
+textbook_path = Path(__file__).parent.absolute() / "textbook.pdf"
 embeddings = OpenAIEmbeddings(openai_api_key=KEYS.OpenAI.api_key)
 
 
 def load_docs() -> list[Document]:
     """Load in the textbook as split documents."""
     splitter = TokenTextSplitter(encoding_name="cl100k_base", chunk_size=450, chunk_overlap=50)
-    loader = TextLoader(str(textbook_path))
-    docs = loader.load()
-    return splitter.split_documents(docs)
+    loader = PyPDFLoader(str(textbook_path))
+    docs = loader.load_and_split(splitter)
+
+    # Append page number to each document content
+    for doc in docs:
+        doc.page_content += f" (Page {doc.metadata.get('page', 'n/a')})"
+
+    return docs
 
 
 def embed_to_file(docs: list[Document]) -> bool:
